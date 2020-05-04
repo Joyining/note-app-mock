cd73112e2fb7f109f6fed6aa9ca51228399181cf
joyining <yining1204@gmail.com>
2020-05-04 14:31:33 +0800

    add AddNote and Editor components

---
+++ src/components/AddNote.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addNote, updateEditingNote } from "../actions";
+import { v4 as uuidv4 } from "uuid";
+import "./style.css";
+
+class AddNote extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  clickAddNote = () => {
+    const { addNote, updateEditingNote } = this.props;
+    const noteId = uuidv4();
+    addNote(noteId, {});
+    updateEditingNote(noteId);
+  };
+
+  render() {
+    return (
+      <div className="" onClick={this.clickAddNote}>
+        Add
+      </div>
+    );
+  }
+}
+
+export default connect(null, { addNote, updateEditingNote })(AddNote);
---
+++ src/components/Editor.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./style.css";
+
+class Editor extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { editingNote } = this.props;
+    return (
+      <div className="editor-wrap">
+        <p>this is editor</p>
+        <p>current editing: {editingNote.toString()}</p>
+        <form>
+          <div>
+            <input id="NoteNext" type="text" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  console.log(state);
+  const editingNote = state.editingNote;
+  console.log(editingNote);
+  return { editingNote };
+};
+
+export default connect(mapStateToProps)(Editor);
