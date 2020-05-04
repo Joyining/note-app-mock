3c2628433a4d02f897c5d6164ab0b4817ca928d3
joyining <yining1204@gmail.com>
2020-05-04 14:14:13 +0800

    remove form in List

--- src/components/List.js
+++ src/components/List.js
@@ -8,46 +8,8 @@ import "./style.css";
 class List extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      showForm: false,
-      formValue: "",
-    };
   }
 
-  inputChange = (e) => {
-    this.setState({ formValue: e.target.value });
-  };
-
-  formSubmit = (e) => {
-    const { formValue } = this.state;
-    const { addNote } = this.props;
-    e.preventDefault();
-    addNote({ title: formValue });
-    this.setState({ formValue: "" });
-  };
-
-  renderForm = () => {
-    const { showForm, formValue } = this.state;
-    if (showForm) {
-      return (
-        <div>
-          <form onSubmit={this.formSubmit}>
-            <div>
-              <i>add</i>
-              <input
-                value={formValue}
-                onChange={this.inputChange}
-                id="NoteNext"
-                type="text"
-              />
-              <label htmlFor="NoteNext">What Next?</label>
-              <button type="submit">submit</button>
-            </div>
-          </form>
-        </div>
-      );
-    }
-  };
   renderNote() {
     const { stateNotes } = this.props;
     console.log(stateNotes);
@@ -68,18 +30,9 @@ class List extends Component {
     this.props.fetchNotes();
   }
   render() {
-    const { showForm } = this.state;
     return (
-      <div>
-        <div>
-          {this.renderForm()}
-          {this.renderNote()}
-        </div>
-        <div>
-          <button onClick={() => this.setState({ showForm: !showForm })}>
-            {showForm ? <i>Close</i> : <i>Add</i>}
-          </button>
-        </div>
+      <div className="list-wrap">
+        <div>{this.renderNote()}</div>
       </div>
     );
   }
