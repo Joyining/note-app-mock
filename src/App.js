d5454ad76d72fa466858fcb612c043708ac417d8
joyining <yining1204@gmail.com>
2020-05-04 14:03:21 +0800

    add Menu and Editor in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Menu from "./components/Menu";
 import List from "./components/List";
+import Editor from "./components/Editor";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app-wrap">
+        <Menu />
         <List />
+        <Editor />
       </div>
     );
   }
---
+++ src/components/Menu.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import AddNote from "./AddNote.js";
+import "./style.css";
+
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="menu-wrap">
+        <AddNote></AddNote>
+      </div>
+    );
+  }
+}
+
+export default Menu;
