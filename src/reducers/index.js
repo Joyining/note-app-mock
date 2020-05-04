f55460ccfdbbcb3f666d1bc44c83a64a68e3a9e0
joyining <yining1204@gmail.com>
2020-05-04 14:30:37 +0800

    add editingNote in combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import notes from "./notes";
+import editingNote from "./editingNote";
 export default combineReducers({
   notes,
+  editingNote,
 });
 // The redux library contains a function named combineReducers that cantake multiple reducers and combine them into one reducer.
