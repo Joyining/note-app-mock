7c5807539980654c3d58881ce4f61bf2767cc2eb
joyining <yining1204@gmail.com>
2020-05-04 14:27:07 +0800

    Revert "add editingNote reducer to pass noteId to update current editing note"
    This reverts commit 334a26813e8dfcc493429c0e89f17550cf576794.

--- src/reducers/editingNote.js
+++
@@ -1,9 +0,0 @@
-import { UPDATE_EDITING_NOTE } from "../actions/types";
-export default (state = {}, action) => {
-  switch (action.type) {
-    case UPDATE_EDITING_NOTE:
-      return action.noteId;
-    default:
-      return state;
-  }
-};
