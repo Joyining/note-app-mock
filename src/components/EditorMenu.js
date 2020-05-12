9a1ae2980152ccefb19c4ebdd7e196c3e937c182
joyining <yining1204@gmail.com>
2020-05-12 10:31:13 +0800

    add key for editor menu item

--- src/components/EditorMenu.js
+++ src/components/EditorMenu.js
@@ -36,7 +36,6 @@ class EditorMenu extends Component {
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
             id="qa-NOTE_ACTIONS"
-            class="_1gRG1Dm3LEqLzpDUzgtvki"
           >
             <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path>
           </svg>
@@ -44,7 +43,7 @@ class EditorMenu extends Component {
         <ul className={showMenu ? "menu-list show" : "menu-list"}>
           {menu.map((item) => {
             return (
-              <li className="menu-item" onClick={item.onClick}>
+              <li key={item.name} className="menu-item" onClick={item.onClick}>
                 {item.name}
               </li>
             );
