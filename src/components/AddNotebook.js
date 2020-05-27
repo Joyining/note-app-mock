a6a1c04f4c745dcdbbfaecad7a3faf4ac2575d8b
joyining <yining1204@gmail.com>
2020-05-27 15:07:14 +0800

    transfer AddNotebook from class component to functional component

--- src/components/AddNotebook.js
+++ src/components/AddNotebook.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 import * as utils from "../utils";
@@ -7,59 +7,42 @@ import LightBoxBg from "./LightBoxBg";
 import LightBoxHeading from "./LightBoxHeading";
 import "../scss/components/addNotebook.scss";
 
-class AddNotebook extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showLightBox: false,
-      notebookName: "",
-      showWarning: false,
-    };
-  }
+const AddNotebook = (props) => {
+  const { addNotebook, cookies, currentUser } = props;
+  const [showLightBox, setShowLightBox] = useState(false);
+  const [notebookName, setNotebookName] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
+
   // 重複了
-  toggleLightBox = (e) => {
-    console.log("toggle light box!!!!!");
-    const { showLightBox } = this.state;
+  const toggleLightBox = (e) => {
     if (e.target === e.currentTarget) {
-      this.setState({
-        showLightBox: showLightBox ? false : true,
-      });
+      setShowLightBox(showLightBox ? false : true);
     }
   };
-  clickAddNotebook = () => {
-    const { showLightBox } = this.state;
-    this.setState({
-      showLightBox: showLightBox ? false : true,
-      notebookName: "",
-      showWarning: false,
-    });
+
+  const clickAddNotebook = () => {
+    setShowLightBox(showLightBox ? false : true);
+    setNotebookName("");
+    setShowWarning(false);
   };
-  getNotebookName = (e) => {
-    this.setState({
-      notebookName: e.target.value,
-    });
-  };
-  confirmAddNotebook = (e) => {
-    const { addNotebook, cookies, currentUser } = this.props;
-    const { notebookName } = this.state;
+
+  const confirmAddNotebook = (e) => {
     const id = uuidv4();
     const getCurrentUser = utils.getCurrentUser(cookies, currentUser);
     if (notebookName) {
       addNotebook(id, getCurrentUser, notebookName);
-      this.toggleLightBox(e);
+      toggleLightBox(e);
     } else {
-      this.setState({
-        showWarning: true,
-      });
+      setShowWarning(true);
     }
   };
-  renderLightBoxMessage = (notebookName, showWarning) => {
-    // const {notebookName} = this.state;
+
+  const renderLightBoxMessage = (notebookName, showWarning) => {
     return (
       <div className="light-box-message-wrap">
         <LightBoxHeading
           headingTitle="Create New Notebook"
-          toggleLightBox={this.toggleLightBox}
+          toggleLightBox={toggleLightBox}
         />
         <p className="message">Name:</p>
         <input
@@ -69,19 +52,18 @@ class AddNotebook extends Component {
           className="input-text"
           placeholder="Notebook Name"
           value={notebookName}
-          onChange={this.getNotebookName}
+          onChange={(e) => {
+            setNotebookName(e.target.value);
+          }}
         ></input>
         <p className={`small-warning-message ${showWarning ? "show" : ""}`}>
           Notebook name is required!
         </p>
         <div className="btn-wrap">
-          <button className="general-btn btn" onClick={this.toggleLightBox}>
+          <button className="general-btn btn" onClick={toggleLightBox}>
             Cancel
           </button>
-          <button
-            className="highlight-btn btn"
-            onClick={this.confirmAddNotebook}
-          >
+          <button className="highlight-btn btn" onClick={confirmAddNotebook}>
             Continue
           </button>
         </div>
@@ -89,24 +71,21 @@ class AddNotebook extends Component {
     );
   };
 
-  render() {
-    const { showLightBox, notebookName, showWarning } = this.state;
-    const parameters = [notebookName, showWarning];
-    return (
-      <div className="add-notebook-wrap">
-        <div className="add-notebook-btn" onClick={this.clickAddNotebook}>
-          New Notebook
-        </div>
-        <LightBoxBg
-          showLightBox={showLightBox}
-          toggleLightBox={this.toggleLightBox}
-          renderLightBoxMessage={this.renderLightBoxMessage}
-          renderParameters={parameters}
-        ></LightBoxBg>
+  const lightBoxParameters = [notebookName, showWarning];
+  return (
+    <div className="add-notebook-wrap">
+      <div className="add-notebook-btn" onClick={clickAddNotebook}>
+        New Notebook
       </div>
-    );
-  }
-}
+      <LightBoxBg
+        showLightBox={showLightBox}
+        toggleLightBox={toggleLightBox}
+        renderLightBoxMessage={renderLightBoxMessage}
+        renderParameters={lightBoxParameters}
+      ></LightBoxBg>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   const currentUser = state.currentUser;
