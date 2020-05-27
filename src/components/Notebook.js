2b5d1225c80e42fccf92e4ab3ac06446c395fe6e
joyining <yining1204@gmail.com>
2020-05-27 17:18:03 +0800

    transfer Notebook to functional component

--- src/components/Notebook.js
+++ src/components/Notebook.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useCallback } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 import * as utils from "../utils";
@@ -9,47 +9,40 @@ import { ReactComponent as NotebookIcon } from "../images/notebook.svg";
 import { ReactComponent as ActionIcon } from "../images/actionHorizontal.svg";
 import "../scss/components/notebookList.scss";
 
-class Notebook extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showMenu: false,
-      expandNotebook: false,
-    };
-  }
-  notebookNameOnClickHandler = (e) => {
-    const { cookies, currentUser, switchView, filterNotes } = this.props;
-    const getCurrentUser = utils.getCurrentUser(cookies, currentUser);
+const Notebook = (props) => {
+  const {
+    cookies,
+    currentUser,
+    switchView,
+    filterNotes,
+    notebookId,
+    setAsDefaultNotebook,
+    thisNotebook,
+    notebook,
+  } = props;
+  const getCurrentUser = utils.getCurrentUser(cookies, currentUser);
+  const [showMenu, setShowMenu] = useState(false);
+  const [expandNotebook, setExpandNotebook] = useState(false);
+
+  const notebookNameOnClickHandler = (e) => {
     const notebookId = e.target.closest("li").id;
     switchView("noteAndEditor");
     filterNotes(getCurrentUser, notebookId);
   };
-  actionOnClickHandler = () => {
-    const { showMenu } = this.state;
-    this.setState({
-      showMenu: showMenu ? false : true,
-    });
+
+  const actionOnClickHandler = () => {
+    setShowMenu(showMenu ? false : true);
   };
-  setAsDefaultOnClickHandler = () => {
-    const {
-      notebookId,
-      cookies,
-      currentUser,
-      setAsDefaultNotebook,
-    } = this.props;
-    const getCurrentUser = utils.getCurrentUser(cookies, currentUser);
+
+  const setAsDefaultOnClickHandler = () => {
     setAsDefaultNotebook(notebookId, getCurrentUser);
   };
 
-  expandNotebook = () => {
-    const { expandNotebook } = this.state;
-    this.setState({
-      expandNotebook: expandNotebook ? false : true,
-    });
+  const expandNotebookOnClickHandler = () => {
+    setExpandNotebook(expandNotebook ? false : true);
   };
 
-  renderNotes = () => {
-    const { notebookId, cookies, thisNotebook } = this.props;
+  const renderNotes = () => {
     console.log("Notebook renderNotes");
     console.log(notebookId);
     let notesInThisNotebook = null;
@@ -87,78 +80,67 @@ class Notebook extends Component {
     }
   };
 
-  componentDidUpdate() {
-    console.log("Notebook componentDidUpdate");
-  }
+  const menu = [
+    {
+      name: "Set as Default Notebook",
+      onClick: setAsDefaultOnClickHandler,
+    },
+  ];
+  return (
+    <li id={notebookId} className="notebook-outer-wrap">
+      <div className="notebook-inner-wrap">
+        <div className="notebook-name-and-icon">
+          <ArrowRight
+            className="expand-icon"
+            onClick={expandNotebookOnClickHandler}
+          />
+          <NotebookIcon className="notebook-icon" />
+          <p className="notebook-name" onClick={notebookNameOnClickHandler}>
+            {notebook.name}
+          </p>
+          <span className="note-count">{`(${notebook.notes.length})`}</span>
+        </div>
 
-  render() {
-    const { notebookId, notebook, cookies } = this.props;
-    const { showMenu, expandNotebook } = this.state;
-    const menu = [
-      {
-        name: "Set as Default Notebook",
-        onClick: this.setAsDefaultOnClickHandler,
-      },
-    ];
-    return (
-      <li id={notebookId} className="notebook-outer-wrap">
-        <div className="notebook-inner-wrap">
-          <div className="notebook-name-and-icon">
-            <ArrowRight className="expand-icon" onClick={this.expandNotebook} />
-            <NotebookIcon className="notebook-icon" />
-            <p
-              className="notebook-name"
-              onClick={this.notebookNameOnClickHandler}
-            >
-              {notebook.name}
-            </p>
-            <span className="note-count">{`(${notebook.notes.length})`}</span>
+        <div className="notebook-detail-and-actions">
+          <div className="note-count">
+            <span>{notebook.notes.length}</span>
+            <span>{` note${notebook.notes.length > 1 ? "s" : ""}`}</span>
           </div>
-
-          <div className="notebook-detail-and-actions">
-            <div className="note-count">
-              <span>{notebook.notes.length}</span>
-              <span>{` note${notebook.notes.length > 1 ? "s" : ""}`}</span>
-            </div>
-            <p className="last-modified-time">
-              {utils.getDisplayedTime(notebook.lastModifiedTime)}
-            </p>
-            {/* <p>{utils.getDisplayedDate(notebook.lastModifiedTime)}</p> */}
-            <p className="is-default-notebook detail-item">
-              {notebook.defaultNotebook.toString() === "true" ? "Default" : ""}
-            </p>
-            <div className={`actions detail-item ${notebookId}`}>
-              <ActionIcon
-                className="icon"
-                onClick={this.actionOnClickHandler}
-              />
-              <ul
-                className={
-                  showMenu ? "shared-menu-list show" : "shared-menu-list"
-                }
-              >
-                {menu.map((item) => {
-                  return (
-                    <li
-                      key={item.name}
-                      className="menu-item"
-                      onClick={item.onClick}
-                    >
-                      {item.name}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+          <p className="last-modified-time">
+            {utils.getDisplayedTime(notebook.lastModifiedTime)}
+          </p>
+          {/* <p>{utils.getDisplayedDate(notebook.lastModifiedTime)}</p> */}
+          <p className="is-default-notebook detail-item">
+            {notebook.defaultNotebook.toString() === "true" ? "Default" : ""}
+          </p>
+          <div className={`actions detail-item ${notebookId}`}>
+            <ActionIcon className="icon" onClick={actionOnClickHandler} />
+            <ul
+              className={
+                showMenu ? "shared-menu-list show" : "shared-menu-list"
+              }
+            >
+              {menu.map((item) => {
+                return (
+                  <li
+                    key={item.name}
+                    className="menu-item"
+                    onClick={item.onClick}
+                  >
+                    {item.name}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
-        <ul className={`note-list ${expandNotebook ? "show" : ""}`}>
-          {this.renderNotes()}
-        </ul>
-      </li>
-    );
-  }
-}
+      </div>
+      <ul className={`note-list ${expandNotebook ? "show" : ""}`}>
+        {renderNotes()}
+      </ul>
+    </li>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => {
   const currentUser = state.currentUser;
