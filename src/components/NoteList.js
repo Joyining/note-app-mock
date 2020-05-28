d515f30b1507b102a272d1fcd97fd122f93f4c87
joyining <yining1204@gmail.com>
2020-05-28 15:12:50 +0800

    temperary hide Notebook on the top of NoteList

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -43,11 +43,11 @@ class NoteList extends Component {
     };
     return (
       <div className="note-list-wrap">
-        <Notebook
+        {/* <Notebook
           notebookId={`${currentNotebook.id}`} // id會跟SideMenu裡的Notebook重複？
           notebook={currentNotebook}
           cookies={cookies}
-        />
+        /> */}
         <div>{this.renderNote()}</div>
       </div>
     );
