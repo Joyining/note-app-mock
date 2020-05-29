3b38d05d6732692209080c8e456339c836f0cfc6
joyining <yining1204@gmail.com>
2020-05-29 10:41:50 +0800

    remove the snap.size condition, defaultNotebook is always false when addNotebook

--- src/actions/addNotebook.js
+++ src/actions/addNotebook.js
@@ -4,26 +4,14 @@ import "@firebase/auth";
 
 export const addNotebook = (id, owner, name = "") => async (dispatch) => {
   const db = firebase.firestore();
-  const defaultNotebook = false;
+  const ref = db.collection("notebooks").doc(id);
   const now = new Date();
-  db.collection("notebooks")
-    .get()
-    .then((snap) => {
-      if (snap.size === 0) {
-        defaultNotebook = true;
-        // 因為第一本notebook在signUp時已建立
-        // 所以這邊可以不需判斷snap.size了
-      }
-    })
-    .then((res) => {
-      const ref = db.collection("notebooks").doc(id);
-      ref.set({
-        createdTime: now,
-        lastModifiedTime: now, // or null?
-        name: name,
-        owner: owner,
-        defaultNotebook: defaultNotebook,
-        notes: [],
-      });
-    });
+  ref.set({
+    createdTime: now,
+    lastModifiedTime: now, // or null?
+    name: name,
+    owner: owner,
+    defaultNotebook: false,
+    notes: [],
+  });
 };
