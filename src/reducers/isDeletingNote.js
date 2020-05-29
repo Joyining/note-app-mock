e17593df5c369dae9556d620f37d34403bfbc096
joyining <yining1204@gmail.com>
2020-05-29 18:00:45 +0800

    remove console.log

--- src/reducers/isDeletingNote.js
+++ src/reducers/isDeletingNote.js
@@ -1,6 +1,5 @@
 import { DELETE_NOTE, FETCH_DATA } from "../actions/types";
 export default (state = {}, action) => {
-  console.log(action.type);
   switch (action.type) {
     case DELETE_NOTE:
     case FETCH_DATA:
