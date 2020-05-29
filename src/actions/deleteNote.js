e631e2dba026caf87cd61192b8c567d81686c7dc
joyining <yining1204@gmail.com>
2020-05-29 10:53:06 +0800

    delete note from notes array in notebook

--- src/actions/deleteNote.js
+++ src/actions/deleteNote.js
@@ -3,37 +3,27 @@ import { firebase } from "@firebase/app";
 import "@firebase/auth";
 import { DELETE_NOTE } from "./types";
 
-export const deleteNote = (noteId) => async (dispatch) => {
+export const deleteNote = (notebookId, noteId) => async (dispatch) => {
   const db = firebase.firestore();
-  const noteRef = db.collection("notes").doc(noteId);
+  const notebookRef = db.collection("notebooks").doc(notebookId);
   const now = new Date();
-  let notebookId = "";
-  let notebookRef = null;
   let notes = null;
   dispatch({
     type: DELETE_NOTE,
     isDeletingNote: true,
   });
 
-  noteRef
-    .get()
-    .then((snapshot) => {
-      notebookId = snapshot.data().notebookId;
-      notebookRef = db.collection("notebooks").doc(notebookId);
-    })
-    .then((res) => {
-      notebookRef.get().then((snapshot) => {
-        notes = snapshot.data().notes;
-        notes.splice(notes.indexOf(noteId), 1);
-        notebookRef.update({
-          notes: notes,
-          lastModifiedTime: now,
-        });
-      });
-    })
-    .then((res) => {
-      noteRef.delete().then(() => {
-        console.log("delete data success");
-      });
+  notebookRef.get().then((snapshot) => {
+    notes = snapshot.data().notes;
+    const targetNote = notes.filter((note) => {
+      note.id === noteId;
     });
+    if (targetNote) {
+      notes.splice(notes.indexOf(targetNote), 1);
+      notebookRef.update({
+        notes: notes,
+        lastModifiedTime: now,
+      });
+    }
+  });
 };
