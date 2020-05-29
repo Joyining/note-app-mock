35d464b5e4e847271adeda1620306a7bbfc53d8b
joyining <yining1204@gmail.com>
2020-05-29 15:26:14 +0800

    modify editNote based on new data structure

--- src/actions/editNote.js
+++ src/actions/editNote.js
@@ -3,45 +3,52 @@ import { firebase } from "@firebase/app";
 import "@firebase/auth";
 import * as utils from "../utils";
 
-export const editNote = (noteId, value, isEditing) => async (dispatch) => {
+export const editNote = (notebookId, noteId, value, isEditing) => async (
+  dispatch
+) => {
   const db = firebase.firestore();
-  const noteRef = db.collection("notes").doc(noteId);
+  const notebookRef = db.collection("notebooks").doc(notebookId);
+  let targetNote = null;
+  let notes = [];
   const now = new Date();
   let title = utils.getNoteTitle(value);
 
-  noteRef.get().then((doc) => {
-    const data = doc.data();
-    const content = data.content;
-    const notebookId = data.notebookId;
-    const notebookRef = db.collection("notebooks").doc(notebookId);
-    if (isEditing === false) {
-      // 如果前一次的Quill onChange是user click Note
-      // 則檢查value是否跟database裡的資料一樣
-      console.log("need to check sameContent");
-      const sameContent = value === content ? true : false;
-      title = data.title ? data.title : ""; // 先初始化
-      if (!sameContent) {
-        console.log("not same");
-        noteRef.update({
-          content: value,
-          lastModifiedTime: now,
-          title: title,
-        });
-        notebookRef.update({
-          lastModifiedTime: now,
-        });
-      } else {
-        console.log("same");
+  notebookRef.get().then((snapshot) => {
+    notes = snapshot.data().notes;
+    for (let note of notes) {
+      if (note.id === noteId) {
+        targetNote = note;
       }
-    } else {
-      noteRef.update({
-        content: value,
-        lastModifiedTime: now,
-        title: title,
-      });
-      notebookRef.update({
-        lastModifiedTime: now,
-      });
+    }
+    if (targetNote) {
+      const content = targetNote.content;
+      if (isEditing === false) {
+        // 如果前一次的Quill onChange是user click Note
+        // 則檢查value是否跟database裡的資料一樣
+        console.log("need to check sameContent");
+        const sameContent = value === content ? true : false;
+        title = targetNote.title ? targetNote.title : ""; // 先初始化
+        if (!sameContent) {
+          console.log("not same");
+          targetNote.content = value;
+          targetNote.lastModifiedTime = now;
+          targetNote.title = title;
+          notebookRef.update({
+            notes: notes,
+            lastModifiedTime: now,
+          });
+        } else {
+          console.log("same");
+        }
+      } else {
+        targetNote.content = value;
+        targetNote.lastModifiedTime = now;
+        targetNote.title = title;
+        notebookRef.update({
+          notes: notes,
+          lastModifiedTime: now,
+        });
+      }
     }
   });
 };
